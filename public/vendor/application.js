2577f056e7ab28e4315ee59a3e02beb146a1548c
Jerry Khalil <khaliljt09@gmail.com>
2015-11-09 17:49:15 -0600

    final web app

--- public/vendor/application.js
+++ public/vendor/application.js
@@ -1,17 +1,57 @@
 $(document).ready(function() {
+  player_hits();
+  player_stays();
+  dealer_hit();
 
-       $(document).on('click', '#hit_form input', function(){
-
-       $.ajax({
-         type: 'POST',
-         url: '/game/player/hit'
-
-       }).done(function(msg) {
-               $('#game').replaceWith(msg);
-
-               });
-
-
-      return false;
-  });
 });
+
+function player_hits () {
+  $(document).on('click', '#hit_form input', function(){
+    alert("player hits!");
+  $.ajax({
+    type: 'POST',
+    url: '/game/player/hit'
+
+  }).done(function(msg) {
+          $('#game').replaceWith(msg);
+
+          });
+
+
+ return false;
+});
+}
+
+function player_stays() {
+  $(document).on('click', '#stay_form input', function(){
+    alert("player stays!");
+  $.ajax({
+    type: 'POST',
+    url: '/game/player/stay'
+
+  }).done(function(msg) {
+          $('#game').replaceWith(msg);
+
+          });
+
+
+ return false;
+});
+}
+
+function dealer_hit() {
+  $(document).on('click', '#dealer_hit input', function(){
+    alert("dealer hits!");
+  $.ajax({
+    type: 'POST',
+    url: '/game/dealer/hit'
+
+  }).done(function(msg) {
+          $('#game').replaceWith(msg);
+
+          });
+
+
+ return false;
+});
+}
